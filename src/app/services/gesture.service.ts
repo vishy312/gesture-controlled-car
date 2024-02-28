3463a5da7cbd690de7c1f70a75b8836fb16d23c2
vishy312 <vishalpal6486@gmail.com>
2024-02-29 00:19:22 +0530

    rewritten gesture service

--- src/app/services/gesture.service.ts
+++ src/app/services/gesture.service.ts
@@ -1,28 +1,42 @@
 import { Injectable } from "@angular/core";
-import * as tf from '@tensorflow/tfjs';
-import { WebcamImage, WebcamUtil } from 'ngx-webcam';
+import * as handpose from '@tensorflow-models/handpose';
+import { BehaviorSubject } from "rxjs";
+
+const GestureMap = {
+    fist: 'stop',
+    victory: 'reverse',
+    finger: 'forward'
+}
+
+type Gesture = 'one' | 'two' | 'stop' | 'none';
+type Direction = 'left' | 'right' | 'none';
+type Size = [number, number];
+type Point = [number, number];
+type Rect = {
+    'topLeft': [number, number],
+    'bottomRight': [number, number]
+ }
 
 @Injectable({
     providedIn: 'root'
 })
 export class GestureService {
 
-    model: any;
-    webcamImage!: WebcamImage;
+    private _swipe$ = new BehaviorSubject<Direction>('none');
+    readonly swipe$ = this._swipe$.asObservable();
 
-    ngOnInit(){
-        this.loadModel();
+    private _gestures$ = new BehaviorSubject<Gesture>('none');
+    readonly gesture$ = this._gestures$.asObservable();
+
+    private _stream!: MediaStream;
+
+    
+    public get stream() : MediaStream {
+        return this._stream;
     }
 
-    async loadModel(){
-        this.model = await tf.loadLayersModel('../../assets/model.json')
+    initializers(canvas: HTMLCanvasElement, video: HTMLVideoElement){
+        
     }
-
-    configureWebcam(){
-        WebcamUtil.getAvailableVideoInputs().then((media)=> this.handleImage(media))
-    }
-
-    handleImage(webcamImage: MediaDeviceInfo[]){
-        console.log(webcamImage);
-    }
+    
 }
